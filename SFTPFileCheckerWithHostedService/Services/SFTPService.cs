88233c58d3bd978b2385c985aa080b4b80865017
Md Sajidur rahman <sajid.ict@gmail.com>
2022-07-30 12:10:04 +0600

    SFTP added

--- SFTPFileCheckerWithHostedService/Services/SFTPService.cs
+++ SFTPFileCheckerWithHostedService/Services/SFTPService.cs
@@ -1,6 +1,26 @@
-﻿namespace SFTPFileCheckerWithHostedService.Services
+﻿using Renci.SshNet;
+
+namespace SFTPFileCheckerWithHostedService.Services
 {
+    public interface ISFTPService
+    {
+        string DownloadFiles();
+    }
     public class SFTPService:ISFTPService
     {
+        public SFTPService()
+        {
+
+        }
+        public string DownloadFiles()
+        {
+            var client = new SftpClient("192.168.0.1", 80, "username", "password");
+            client.Connect();
+            using (Stream fileStream = File.OpenWrite("c:\\temp\\file.tmp"))
+            {
+                client.DownloadFile("", fileStream);
+            }
+            return "";
+        }
     }
 }
